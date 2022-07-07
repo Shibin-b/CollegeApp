46090eb16fbc6d37a3109aa4690e114b834bcc5a
Shibin-b <shibinbevin123@gmail.com>
2022-07-07 17:22:33 +0530

    DeptName component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeptNameComponent } from './dept-name/dept-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeptNameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dept-name/dept-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dept-name',
+  templateUrl: './dept-name.component.html',
+  styleUrls: ['./dept-name.component.css']
+})
+export class DeptNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
